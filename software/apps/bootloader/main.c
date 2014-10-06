5eb667bb8247a17f39bae4fd10c9559373c7202a
Brad Campbell <bradjc5@gmail.com>
2014-10-06 18:56:58 -0400

    added serial bootloader app
    this gets flashed separately

---
+++ software/apps/bootloader/main.c
@@ -0,0 +1,227 @@
+/**@file
+ *
+ * UART Bootloader
+ *
+ * -# Receive start data packet.
+ * -# Based on start packet, prepare NVM area to store received data.
+ * -# Receive data packet.
+ * -# Validate data packet.
+ * -# Write Data packet to NVM.
+ * -# If not finished - Wait for next packet.
+ * -# Receive stop data packet.
+ * -# Activate Image, boot application.
+ *
+ */
+#include "dfu_transport.h"
+#include "bootloader.h"
+#include "bootloader_util.h"
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+#include "nordic_common.h"
+#include "nrf.h"
+#include "nrf_soc.h"
+#include "app_error.h"
+#include "nrf_gpio.h"
+#include "nrf51_bitfields.h"
+#include "ble.h"
+#include "nrf51.h"
+#include "ble_hci.h"
+#include "app_scheduler.h"
+#include "app_timer.h"
+#include "app_gpiote.h"
+#include "nrf_error.h"
+#include "squall.h"
+#include "ble_debug_assert_handler.h"
+#include "softdevice_handler.h"
+#include "pstorage_platform.h"
+#include "nrf_mbr.h"
+
+#define APP_GPIOTE_MAX_USERS            1                                                       /**< Number of GPIOTE users in total. Used by button module and dfu_transport_serial module (flow control). */
+
+#define APP_TIMER_PRESCALER             0                                                       /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_MAX_TIMERS            3                                                       /**< Maximum number of simultaneously created timers. */
+#define APP_TIMER_OP_QUEUE_SIZE         4                                                       /**< Size of timer operation queues. */
+
+#define BUTTON_DETECTION_DELAY          APP_TIMER_TICKS(50, APP_TIMER_PRESCALER)                /**< Delay from a GPIOTE event until a button is reported as pushed (in number of timer ticks). */
+
+#define SCHED_MAX_EVENT_DATA_SIZE       MAX(APP_TIMER_SCHED_EVT_SIZE, 0)                        /**< Maximum size of scheduler events. */
+
+#define SCHED_QUEUE_SIZE                20                                                      /**< Maximum number of events in the scheduler queue. */
+
+
+/**@brief Function for error handling, which is called when an error has occurred.
+ *
+ * @warning This handler is an example only and does not fit a final product. You need to analyze
+ *          how your product is supposed to react in case of error.
+ *
+ * @param[in] error_code  Error code supplied to the handler.
+ * @param[in] line_num    Line number where the handler is called.
+ * @param[in] p_file_name Pointer to the file name.
+ */
+void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p_file_name)
+{
+    // This call can be used for debug purposes during application development.
+    // @note CAUTION: Activating this code will write the stack to flash on an error.
+    //                This function should NOT be used in a final product.
+    //                It is intended STRICTLY for development/debugging purposes.
+    //                The flash write will happen EVEN if the radio is active, thus interrupting
+    //                any communication.
+    //                Use with care. Un-comment the line below to use.
+    // ble_debug_assert_handler(error_code, line_num, p_file_name);
+
+    // On assert, the system can only recover on reset.
+    NVIC_SystemReset();
+}
+
+
+/**@brief Callback function for asserts in the SoftDevice.
+ *
+ * @details This function will be called in case of an assert in the SoftDevice.
+ *
+ * @warning This handler is an example only and does not fit a final product. You need to analyze
+ *          how your product is supposed to react in case of Assert.
+ * @warning On assert from the SoftDevice, the system can only recover on reset.
+ *
+ * @param[in] line_num    Line number of the failing ASSERT call.
+ * @param[in] file_name   File name of the failing ASSERT call.
+ */
+void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
+{
+    app_error_handler(0xDEADBEEF, line_num, p_file_name);
+}
+
+/**@brief Function for initializing the GPIOTE handler module.
+ */
+static void gpiote_init(void)
+{
+    APP_GPIOTE_INIT(APP_GPIOTE_MAX_USERS);
+}
+
+
+/**@brief Function for initializing the timer handler module (app_timer).
+ */
+static void timers_init(void)
+{
+    // Initialize timer module, making it use the scheduler.
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_MAX_TIMERS, APP_TIMER_OP_QUEUE_SIZE, true);
+}
+
+
+/**@brief Function for initializing the button module.
+ */
+static void buttons_init(void)
+{
+    nrf_gpio_cfg_sense_input(BOOTLOADER_CTRL_PIN,
+                             BOOTLOADER_CTRL_PULL,
+                             NRF_GPIO_PIN_SENSE_LOW);
+
+}
+
+
+/**@brief Function for dispatching a BLE stack event to all modules with a BLE stack event handler.
+ *
+ * @details This function is called from the scheduler in the main loop after a BLE stack
+ *          event has been received.
+ *
+ * @param[in]   p_ble_evt   Bluetooth stack event.
+ */
+static void sys_evt_dispatch(uint32_t event)
+{
+    pstorage_sys_event_handler(event);
+}
+
+
+/**@brief Function for initializing the soft device/chip.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ *
+ * @param[in] init_softdevice  true if SoftDevice should be initialized. The SoftDevice must only
+ *                             be initialized if a chip reset has occured. Soft reset from
+ *                             application must not reinitialize the SoftDevice.
+ */
+static void softdevice_init(bool init_softdevice)
+{
+    uint32_t         err_code;
+    sd_mbr_command_t com = {SD_MBR_COMMAND_INIT_SD, };
+
+    if (init_softdevice) {
+        err_code = sd_mbr_command(&com);
+        APP_ERROR_CHECK(err_code);
+    }
+
+    err_code = sd_softdevice_vector_table_base_set(BOOTLOADER_REGION_START);
+    APP_ERROR_CHECK(err_code);
+
+    SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, true);
+
+}
+
+
+/**@brief Function for event scheduler initialization.
+ */
+static void scheduler_init(void) {
+    APP_SCHED_INIT(SCHED_MAX_EVENT_DATA_SIZE, SCHED_QUEUE_SIZE);
+}
+
+
+/**@brief Function for bootloader main entry.
+ */
+int main(void)
+{
+    uint32_t err_code;
+    bool     dfu_start = false;
+    bool     app_reset = (NRF_POWER->GPREGRET == BOOTLOADER_DFU_START);
+
+    // This check ensures that the defined fields in the bootloader corresponds with actual
+    // setting in the nRF51 chip.
+    APP_ERROR_CHECK_BOOL(*((uint32_t *)NRF_UICR_BOOT_START_ADDRESS) == BOOTLOADER_REGION_START);
+    APP_ERROR_CHECK_BOOL(NRF_FICR->CODEPAGESIZE == CODE_PAGE_SIZE);
+
+    // Initialize.
+    timers_init();
+    gpiote_init();
+    buttons_init();
+    bootloader_init();
+
+    // Check if we reset in the middle of a firmware update
+    if (bootloader_dfu_sd_in_progress()) {
+        err_code = bootloader_dfu_sd_update_continue();
+        APP_ERROR_CHECK(err_code);
+
+        softdevice_init(!app_reset);
+        scheduler_init();
+
+        err_code = bootloader_dfu_sd_update_finalize();
+        APP_ERROR_CHECK(err_code);
+
+    } else {
+        // If stack is present then continue initialization of bootloader.
+        softdevice_init(!app_reset);
+        scheduler_init();
+    }
+
+    // Check if the Bootloader Control pin is low. If so, enter the bootloader
+    // mode.
+    dfu_start = (nrf_gpio_pin_read(BOOTLOADER_CTRL_PIN) == 0);
+
+    // If the Bootloader Control pin is low or the application in the flash
+    // is not valid, enter the bootloader mode.
+    if (dfu_start || (!bootloader_app_is_valid(DFU_BANK_0_REGION_START))) {
+        err_code = sd_power_gpregret_clr(POWER_GPREGRET_GPREGRET_Msk);
+        APP_ERROR_CHECK(err_code);
+
+        // Initiate an update of the firmware.
+        err_code = bootloader_dfu_start();
+        APP_ERROR_CHECK(err_code);
+    }
+
+    // If the application was or now is valid, run it
+    if (bootloader_app_is_valid(DFU_BANK_0_REGION_START)) {
+        // Select a bank region to use as application region.
+        // @note: Only applications running from DFU_BANK_0_REGION_START is supported.
+        bootloader_app_start(DFU_BANK_0_REGION_START);
+    }
+
+    NVIC_SystemReset();
+}
