31ece366791862523120b03d418bb63d2120b286
Brad Campbell <bradjc5@gmail.com>
2015-06-13 02:14:02 -0400

    torch updates

--- software/src/torch.c
+++ software/src/torch.c
@@ -1,16 +1,14 @@
-
 #include "torch.h"
-
-#include "twi_master.h"
 
 #include "led.h"
 
-bool platform_init () {
-	bool err;
+uint8_t MAC_ADDR[6] = {0x00, 0x00, 0x50, 0xe5, 0x98, 0xc0};
 
+void platform_init () {
 
-	err = twi_master_init();
+	// Setup the interrupt pin to the CC2538
+	nrf_gpio_cfg_output(INTERRUPT_PIN);
+    nrf_gpio_pin_clear(INTERRUPT_PIN);
 
-	return err;
 }
 
