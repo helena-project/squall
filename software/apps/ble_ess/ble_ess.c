33d00d075569cfdb73e107cce2623aa50e094579
Deepika <dnataraj@umich.edu>
2015-06-25 16:26:56 -0700

    Updated UUIDS and made generic add char function

--- software/apps/ble_ess/ble_ess.c
+++ software/apps/ble_ess/ble_ess.c
@@ -132,6 +132,80 @@ void ble_ess_on_ble_evt(ble_ess_t * p_ess, ble_evt_t * p_ble_evt)
 }
 
 
+/**@brief Function for adding a characteristic.
+
+ *
+
+ * @param[in]   p_ess        Environmental Service structure.
+
+ * @param[in]   p_ess_init   Information needed to initialize the service.
+
+ *
+
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+
+ */
+
+static uint32_t ess_char_add(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_init, uint32_t ess_char_uuid, ble_gatts_char_handles_t *const ess_char_handles)
+
+{
+
+    ble_gatts_char_md_t char_md;
+    ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
+    ble_gatts_attr_md_t attr_md;
+
+    memset(&char_md, 0, sizeof(char_md));
+
+    char_md.char_props.read   = 1;
+    char_md.char_props.write   = 0;
+    char_md.char_props.notify = 0;
+    char_md.p_char_user_desc  = NULL;
+    char_md.p_char_pf         = NULL;
+    char_md.p_user_desc_md    = NULL;
+    //char_md.p_cccd_md         = &cccd_md;
+    char_md.p_sccd_md         = NULL;
+
+    ble_uuid.type = p_ess->uuid_type;
+    ble_uuid.uuid = ess_char_uuid;
+    //ble_uuid.uuid = 0x2A1C;
+
+
+    memset(&attr_md, 0, sizeof(attr_md));
+
+
+// According to ESS_SPEC_V10, the read operation on cccd should be possible without
+// authentication.
+
+//BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+//BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&attr_md.write_perm); 
+    attr_md.vloc       = BLE_GATTS_VLOC_STACK;
+    attr_md.rd_auth    = 0;
+    attr_md.wr_auth    = 0;
+    attr_md.vlen       = 0;
+
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+    attr_char_value.p_uuid    = &ble_uuid;
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.init_len  = sizeof(uint8_t);
+    attr_char_value.init_offs = 0;
+    attr_char_value.max_len   = sizeof(uint8_t);
+    attr_char_value.p_value   = NULL;
+
+
+    return sd_ble_gatts_characteristic_add(p_ess->service_handle, &char_md,
+                                            &attr_char_value,
+                                            ess_char_handles);
+
+
+}
+
+
+
 
 
 
@@ -148,7 +222,7 @@ void ble_ess_on_ble_evt(ble_ess_t * p_ess, ble_evt_t * p_ble_evt)
  * @return      NRF_SUCCESS on success, otherwise an error code.
 
  */
-
+ /*
 static uint32_t temp_char_add(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_init)
 
 {
@@ -206,7 +280,7 @@ static uint32_t temp_char_add(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_in
 
 
 }
-
+*/
 
 
 /**@brief Function for adding the Pressure characteristic.
@@ -222,7 +296,7 @@ static uint32_t temp_char_add(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_in
  * @return      NRF_SUCCESS on success, otherwise an error code.
 
  */
-
+/*
 static uint32_t pres_char_add(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_init)
 
 {
@@ -278,7 +352,7 @@ static uint32_t pres_char_add(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_in
 
 
 }
-
+*/
 
 
 
@@ -328,6 +402,27 @@ uint32_t ble_ess_init(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_init)
 
     }
 
+    err_code = ess_char_add(p_ess, p_ess_init, ESS_UUID_TEMP_CHAR, &p_ess->temp_char_handles);
+
+    if (err_code != NRF_SUCCESS)
+
+    {
+
+        return err_code;
+
+    }
+
+    err_code = ess_char_add(p_ess, p_ess_init, ESS_UUID_PRES_CHAR, &p_ess->pres_char_handles);
+
+    if (err_code != NRF_SUCCESS)
+
+    {
+
+        return err_code;
+
+    }
+
+    /*
     err_code = temp_char_add(p_ess, p_ess_init);
 
     if (err_code != NRF_SUCCESS)
@@ -350,7 +445,7 @@ uint32_t ble_ess_init(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_init)
 
     }
 
-    
+    */
 
     return NRF_SUCCESS;
 
