94a05db26b1e1120fdd560633c6725fb10656f28
Deepika <dnataraj@umich.edu>
2015-06-29 20:55:30 -0400

    Added some UUIDs

--- software/apps/ble_ess/ble_ess.h
+++ software/apps/ble_ess/ble_ess.h
@@ -1,79 +1,3 @@
-/* Copyright (c) 2012 Nordic Semiconductor. All Rights Reserved.
-
- *
-
- * The information contained herein is property of Nordic Semiconductor ASA.
-
- * Terms and conditions of usage are described in detail in NORDIC
-
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
-
- *
-
- * Licensees are granted free, non-transferable use of the information. NO
-
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
-
- * the file.
-
- *
-
- */
-
-
-
-/** @file
-
- *
-
- * @defgroup ble_sdk_srv_ess Battery Service
-
- * @{
-
- * @ingroup ble_sdk_srv
-
- * @brief Battery Service module.
-
- *
-
- * @details This module implements the Battery Service with the Battery Level characteristic.
-
- *          During initialization it adds the Battery Service and Battery Level characteristic
-
- *          to the BLE stack dataesse. Optionally it can also add a Report Reference descriptor
-
- *          to the Battery Level characteristic (used when including the Battery Service in
-
- *          the HID service).
-
- *
-
- *          If specified, the module will support notification of the Battery Level characteristic
-
- *          through the ble_ess_battery_level_update() function.
-
- *          If an event handler is supplied by the application, the Battery Service will
-
- *          generate Battery Service events to the application.
-
- *
-
- * @note The application must propagate BLE stack events to the Battery Service module by calling
-
- *       ble_ess_on_ble_evt() from the @ref softdevice_handler callback.
-
- *
-
- * @note Attention!
-
- *  To maintain compliance with Nordic Semiconductor ASA Bluetooth profile
-
- *  qualification listings, this section of source code must not be modified.
-
- */
-
-
-
 #ifndef BLE_ESS_H__
 
 #define BLE_ESS_H__
@@ -99,9 +23,26 @@
 
 #define ESS_UUID_HUM_CHAR 0x2A6F
 
+#define INIT_TEMP_LEN 2
+
+#define MAX_TEMP_LEN 2
+
+#define INIT_PRES_LEN 4
+
+#define MAX_PRES_LEN 4
+
+#define INIT_HUM_LEN 2
+
+#define MAX_HUM_LEN 2
+
+#define ESS_UUID_ES_MEAS_DESC 0x290C
+
 
 // Forward declaration of the ble_ess_t type.
 typedef struct ble_ess_s ble_ess_t;
+
+typedef struct ess_meas_des_s ess_meas_des_t;
+
 
 
 /**@brief ESS event handler type. */
@@ -114,79 +55,71 @@ typedef void (*ble_ess_evt_handler_t) (ble_ess_t * p_ess, uint8_t new_state);
 typedef struct
 {
     ble_ess_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the ESS. */
+    bool						  is_notify_supported;		/**< Determines if notifications are supported */
+
 } ble_ess_init_t;
 
 
 /**@brief ESS structure. This contains various status information for the service. */
 typedef struct ble_ess_s
 {
-    //ble_ess_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the ESS. */
-    uint16_t                      ess_service_handle;                 /**< Handle of ESS (as provided by the BLE stack). */
+    ble_ess_evt_handler_t         evt_handler;              /**< Event handler to be called for handling events in the ESS. */
+    bool						  is_notify_supported;		/**< TRUE if notifications are supported */
+    uint16_t                      ess_service_handle;         /**< Handle of ESS (as provided by the BLE stack). */
     ble_gatts_char_handles_t      temp_char_handles;          /**< Handles related to the Temperature characteristic. */
 	ble_gatts_char_handles_t      pres_char_handles;          /**< Handles related to the Pressure characteristic. */
-	ble_gatts_char_handles_t      hum_char_handles;          /**< Handles related to the Humidity characteristic. */
-	uint8_t                     uuid_type;
-	//uint8_t                       battery_level_last;             /**< Last Battery Level measurement passed to the Battery Service. */
-    uint16_t                      conn_handle;                    /**< Handle of the current connection (as provided by the BLE stack, is BLE_CONN_HANDLE_INVALID if not in a connection). */
-
+	//ble_gatts_char_handles_t      pres_char_desc_handles;          /**< Handles related to the Pressure characteristic. */
+	ble_gatts_char_handles_t      hum_char_handles;           /**< Handles related to the Humidity characteristic. */
+	uint8_t                       uuid_type;
+    uint16_t                      conn_handle;                /**< Handle of the current connection (as provided by the BLE stack, is BLE_CONN_HANDLE_INVALID if not in a connection). */
+    
 } ble_ess_t;
 
-
-/**@brief ES Measurement Descriptor */
+/**@brief ESS Measurement Descriptor */
 // This is only mandatory is multiple instances of an ESS Characteristic with the same UUID are supported; Otherwise, it is optional.
-/*
-typedef struct
-{
-    //insert bitfield for flags
-    uint8_t samp_func; // sampling function
-    uint24_t meas_per; //measurement period
-    uint24_t up_intv; //update interval
-    uint8_t app; // application
-    uint8_t	meas_unc; //measurement uncertainty
 
-} es_meas_des;
-*/
+ typedef struct ess_meas_des_s
+ {
+ 	ble_uuid_t* desc_uuid;
+ 	int flags : 16; // insert bitfield for field
+ 	uint8_t samp_func; // sampling function
+ 	unsigned int meas_per : 24; //measurement period
+ 	unsigned int up_intv : 24; //update interval
+ 	uint8_t app; // application
+ 	uint8_t	meas_unc; //measurement uncertainty
+ } ess_meas_desc_t;
+ 
 
 
-/**@brief ES Trigger Setting Descriptor */
+/**@brief ESS Trigger Setting Descriptor */
 //This is mandatory if notifications are supported. Otherwise, it is excluded.
 //Since notifications are optional, and in this version its function is not implemented, this descriptor will also not be implemented.
 /*
-typedef struct
-{
-    //octet?!
+ typedef struct
+ {
+ //octet?!
+ } ess_trig_set_des;
+ */
 
-} es_trig_set_des;
-*/
-
-/**@brief ES Configuration Descriptor */
+/**@brief ESS Configuration Descriptor */
 // Mandatory if multiple ES Trigger Setting descriptors are supported; excluded otherwise.
 // This this is a C2/C3 requirement, and notifications are not implemented in this version, this descriptor will also not be implemented.
 /*
-typedef struct
-{
-    //octet?!
+ typedef struct
+ {
+ //octet?!
+ } ess_config_des;
+ */
 
-} es_config_des;
-*/
 
-
-/**@brief Function for initializing the ESSe.
-
+/**@brief Function for initializing the ESS.
  *
-
  * @param[out]  p_ess       ESS structure. This structure will have to be supplied by
-
  *                          the application. It will be initialized by this function, and will later
-
  *                          be used to identify this particular service instance.
-
  * @param[in]   p_ess_init  Information needed to initialize the service.
-
  *
-
  * @return      NRF_SUCCESS on successful initialization of service, otherwise an error code.
-
  */
 
 uint32_t ble_ess_init(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_init);
@@ -196,39 +129,24 @@ uint32_t ble_ess_init(ble_ess_t * p_ess, const ble_ess_init_t * p_ess_init);
 
 
 /**@brief Environmental Sensing Service event. */
-
 //typedef struct
-
 //{
-
-  //  ble_ess_evt_type_t evt_type;                                  /**< Type of event. */
-
+//  ble_ess_evt_type_t evt_type;         /**< Type of event. */
 //} ble_ess_evt_t;
 
 
 
 /**@brief Function for handling the Application's BLE Stack events.
-
  *
-
  * @details Handles all events from the BLE stack of interest to the Battery Service.
-
  *
-
  * @note For the requirements in the ESS specification to be fulfilled,
-
  *       ble_ess_battery_level_update() must be called upon reconnection if the
-
  *       battery level has changed while the service has been disconnected from a bonded
-
  *       client. ~ *~ *~ *~ ~* ~IGNORE *~*~*~*~ *~ *~
-
  *
-
  * @param[in]   p_ess      ESS structure.
-
  * @param[in]   p_ble_evt  Event received from the BLE stack.
-
  */
 
 void ble_ess_on_ble_evt(ble_ess_t * p_ess, ble_evt_t * p_ble_evt);
