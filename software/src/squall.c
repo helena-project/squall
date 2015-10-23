e18807be25e26a6016de525475b167339bf667d3
Brad Campbell <bradjc5@gmail.com>
2015-10-23 17:32:57 -0400

    beacon app working with new infrastructure

--- software/src/squall.c
+++ software/src/squall.c
@@ -1,19 +1,3 @@
 
 #include "squall.h"
-
-#include "twi_master.h"
-
 #include "led.h"
-
-uint8_t MAC_ADDR[6] = {0x00, 0x00, 0x50, 0xe5, 0x98, 0xc0};
-
-bool platform_init () {
-	bool err;
-
-	led_init(LED_0);
-
-	err = twi_master_init();
-
-	return err;
-}
-
